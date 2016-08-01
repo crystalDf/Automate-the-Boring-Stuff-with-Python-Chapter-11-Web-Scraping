99fa4bdc1db7d52ee2d18facf9c9bff1fe15531a
crystalDf <chendong333@gmail.com>
2016-08-01 23:08:39 +0800

    Chapter 11 Web Scraping

--- requests_test.py
+++ requests_test.py
@@ -9,9 +9,18 @@ print(len(res.text))
 
 print(res.text[:250])
 
-res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
+# res = requests.get('http://inventwithpython.com/page_that_does_not_exist')
 
 try:
     res.raise_for_status()
+    play_file = open('RomeoAndJuliet.txt', 'wb')
+    for chunk in res.iter_content(100000):
+        print(play_file.write(chunk))
+    play_file.close()
 except Exception as exc:
     print('There was a problem: %s' % exc)
+
+# The iter_content() method returns “chunks” of the content on each iteration
+# through the loop. Each chunk is of the bytes data type, and you get to
+# specify how many bytes each chunk will contain. One hundred thousand bytes
+# is generally a good size, so pass 100000 as the argument to iter_content().
