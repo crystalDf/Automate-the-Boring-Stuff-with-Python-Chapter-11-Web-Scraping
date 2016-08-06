2cdf09d53f439a61bed76c93308f72e900fe456e
crystalDf <chendong333@gmail.com>
2016-08-06 17:15:25 +0800

    Chapter 11 Web Scraping

--- download_xkcd.py
+++ download_xkcd.py
@@ -9,11 +9,33 @@ url = 'http://xkcd.com' # starting url
 os.makedirs('xkcd', exist_ok=True)  # store comics in ./xkcd
 
 while not url.endswith('#'):
-# TODO: Download the page.
-# TODO: Find the URL of the comic image.
-# TODO: Download the image.
-# TODO: Save the image to ./xkcd.
-# TODO: Get the Prev button's url.
-    break
+    # Download the page.
+    print('Downloading page %s...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text)
+
+    # Find the URL of the comic image.
+    comic_elem = soup.select('#comic img')
+    if not comic_elem:
+        print('Could not find comic image.')
+    else:
+        comic_url = "http:" + comic_elem[0].get('src')
+        # Download the image.
+        print('Downloading image %s...' % comic_url)
+        res = requests.get(comic_url)
+        res.raise_for_status()
+
+        # Save the image to ./xkcd.
+        image_file = open(os.path.join('xkcd', os.path.basename(comic_url)),
+                          'wb')
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+
+    # Get the Prev button's url.
+    pre_link = soup.select('a[rel="prev"]')[0]
+    url = 'http://xkcd.com' + pre_link.get('href')
 
 print('Done.')
