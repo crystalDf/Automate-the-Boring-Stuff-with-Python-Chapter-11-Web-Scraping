a178e31d3c6d0d6ca696a683579aefb503f11c8b
crystalDf <chendong333@gmail.com>
2016-08-08 23:22:42 +0800

    Chapter 11 Web Scraping

--- selenium_test.py
+++ selenium_test.py
@@ -11,3 +11,7 @@ try:
     print('Found <%s> element with that class name!' % elem.tag_name)
 except:
     print('Was not able to find an element with that name.')
+
+link_elem = browser.find_element_by_link_text('Read It Online')
+print(type(link_elem))
+link_elem.click()
