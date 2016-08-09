dc1b201bef120dc536cf22acc70a06c0f603f526
crystalDf <chendong333@gmail.com>
2016-08-09 21:58:30 +0800

    Chapter 11 Web Scraping

--- selenium_test.py
+++ selenium_test.py
@@ -15,3 +15,10 @@ except:
 link_elem = browser.find_element_by_link_text('Read It Online')
 print(type(link_elem))
 link_elem.click()
+
+browser.get('https://gmail.com')
+email_elem = browser.find_element_by_id('Email')
+email_elem.send_keys('not_my_real_email@gmail.com')
+password_elem = browser.find_element_by_id('Passwd')
+password_elem.send_keys('12345')
+password_elem.submit()
